98b08978e6e52aff9dfe9453abb293b0900347e1
Nino3103 <jeangilardo@gmail.com>
2019-04-08 19:28:48 +0200

    tentative de finir le dernier exo

---
+++ lib/06_pig_latin.rb
@@ -0,0 +1,37 @@
+def translate(word)
+
+def vowel(element)
+  if element[0] == ("a" || "e" || "i" || "u" || "o")
+    return element.insert(-1, "ay")
+  end
+end
+
+def consonant(element)
+  if element[0] && element[1] != ("a" || "e" || "i" || "o" || "u" || "y")
+  		return element.delete(element[0..1]) + element[0..1] + "ay"
+  end
+end
+
+def doubleconsonant(element)
+  if element[0] != ("a" || "e" || "i" || "o" || "u" || "y")
+  		return element.reverse + "ay"
+  end
+end
+
+def single(element)
+	if element[0.. 1] == "sc"
+		return element[3.. element.size - 1] + element[0.. 2] + "ay"
+	elsif element[0.. 1] == "qu"
+		return element[2.. element.size - 1] + element[0.. 1] + "ay"
+  end
+end
+
+  def consonant(element)
+  	if element.match(/[qu]/)
+  		return element[(element.size/2).. element.size] + element[0.. (element.size/2)-1] + "ay"
+  	end
+  end
+
+  return consonant("square")
+
+end
